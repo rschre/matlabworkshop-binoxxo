af0348799d439e5616b58692aa5179d1f744fe5f
Raffael Schreiber <49059499+rschre@users.noreply.github.com>
2022-12-04 16:41:01 +0100

    modified triplet detection

--- Grid.m
+++ Grid.m
@@ -23,37 +23,70 @@ classdef Grid
 
         function verifyGrid(obj, app)
             expression = "(X{3,})|(O{3,})"; % Matches 3 or more of either X or O in a row
-            gridCorrect = true;
 
-            disp(obj.values)
             gridFilled = ~any(strcmp(obj.values, ""), "all");
+
+            errorList = strings(0);
+            gridHasErrors = false;
 
             if not(gridFilled)
                 uialert(app.UIFigure,"Fill out all fields before checking", "Input incomplete")
                 return
             end
 
-            [colsCorrect, errorCols] = verifyCols(obj, expression);
-            if not(colsCorrect)
-                disp("Error on column number(s): ");
-                uialert(app.UIFigure,"Error on column number: "+errorCols, "Input incorrect")
-                disp(errorCols);
-                gridCorrect = false;
+            [tripletRows, tripletCols] = checkTriplets(obj, expression);
+            if not(isempty(tripletRows))
+                errorList(end+1) = "Triplet on row number: "+tripletRows;
+                gridHasErrors = true;
             end
-            
-            [rowsCorrect, errorRows] = verifyRows(obj, expression);
-            if not(rowsCorrect)
-                disp("Error on row number(s): ");
-                uialert(app.UIFigure,"Error on row number: "+errorRows, "Input incorrect")
-                disp(errorRows);
-                gridCorrect = false;
+            if not(isempty(tripletCols))
+                errorList(end+1) = "Triplet on column number: "+tripletCols;
+                gridHasErrors = true;
             end
 
-            if gridCorrect
+
+%             [colsCorrect, errorCols] = verifyCols(obj, expression);
+%             if not(colsCorrect)
+%                 uialert(app.UIFigure,"Error on column number: "+errorCols, "Input incorrect")
+%                 gridHasErrors = true;
+%             end
+%             
+%             [rowsCorrect, errorRows] = verifyRows(obj, expression);
+%             if not(rowsCorrect)
+%                 uialert(app.UIFigure,"Error on row number: "+errorRows, "Input incorrect")
+%                 gridHasErrors = true;
+%             end
+
+            if gridHasErrors
+                disp(errorList)
+                uialert(app.UIFigure, errorList, "Input incorrect")
+            else
                 uialert(app.UIFigure,"You did it!", "Success", "Icon", "success")
-                disp("You did it!")
             end
 
+        end
+
+        function [tripletRows, tripletCols] = checkTriplets(obj, expression)
+            tripletRows = [];
+            tripletCols = [];
+            
+            % Join every row to a single string, check regex
+            for i = 1:obj.size
+                row = obj.values(i,:).join("");
+                res = regexp(row, expression);
+                if(length(res) >= 1)
+                    tripletRows(end+1) = i; %#ok<AGROW> 
+                end
+            end
+
+            % Same for columns
+            for i = 1:obj.size
+                col = obj.values(:,i).join("");
+                res = regexp(col, expression);
+                if(length(res) >= 1)
+                    tripletCols(end+1) = i; %#ok<AGROW> 
+                end
+            end
         end
 
         function [colsCorrect, errorCols] = verifyCols(obj, expression)
